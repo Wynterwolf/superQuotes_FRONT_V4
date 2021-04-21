d2e9b01fce5770b731062f8b1a5218eaa69e38a2
Samantha Jordan <shinyguns@gmail.com>
2021-04-21 18:12:23 -0500

    added classes

---
+++ src/quotes.js
@@ -0,0 +1,9 @@
+class Quote{
+    constructor(quote, character, episode){
+    this.quote = quote
+    this.character = character
+    this.episode = episode
+    }
+// instance method for rendering quote
+
+}
---
+++ src/user.js
@@ -0,0 +1,8 @@
+class User{
+    constructor(name, username, email){
+        this.name = name
+        this.username = username
+        this.email = email
+    }
+    // method for rendering user information
+}
