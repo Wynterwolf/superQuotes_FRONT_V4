1cadc7066a682a3b60db45225831024a3d67f20d
Samantha Jordan <shinyguns@gmail.com>
2021-04-21 18:20:17 -0500

    added two fetch

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    fetchUsers()
 })
+
+const BASE_URL = ("http://127.0.0.1:3000/")
+
+
+    function fetchUsers(){
+    fetch(`${BASE_URL}/quotes`)
+    .then(resp => resp.json())
+    .then(users => {
+        console.log(users)
+
+    })
+    }
+
+    function fetchQuotes(){
+        fetch(`${BASE_URL}/quotes`)
+        .then(resp => resp.json())
+        .then(quotes => {
+      // This for random quote setup
+          const quote = quotes[Math.floor(Math.random() * quotes.length)]
+        u = new Quote(quote.quote)
+        u.renderQuote()
+    })
+    }
